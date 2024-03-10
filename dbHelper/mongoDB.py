187a0d9a0a03cac361c7c5cdadf6aae264661132
Ali Hashempour <ali.hashempour@gmail.com>
2024-03-10 18:20:54 +0330

    feat: update songId function

--- dbHelper/mongoDB.py
+++ dbHelper/mongoDB.py
@@ -11,3 +11,16 @@ def connect_to_mongo():
 def insert_to_db(_id, user_email, status="pending"):
     collection = connect_to_mongo()
     collection.insert_one({"_id": _id, "email": user_email, "status": status, "songId": None})
+
+
+def update_songId(_id, song_id):
+    collection = connect_to_mongo()
+    update = {
+        "$set": {
+            "songId": song_id,
+            "status": "ready"
+        }
+    }
+    result = collection.update_one({'_id': _id}, update=update)
+    if result.modified_count > 0:
+        print(f"Document {_id} updated successfully.")
