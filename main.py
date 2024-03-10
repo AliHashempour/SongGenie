3138463f2cc589033457465ab4c729d52629dce4
Ali Hashempour <ali.hashempour@gmail.com>
2024-03-10 11:00:29 +0330

    feat: add first service

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-import hashlib
-import logging
-
-from flask import Flask, render_template, request, redirect
-import sqlite3
-import string
-import random
+from flask import Flask, request
+from utils.general import generate_hash, save_file
+from dbHelper.mongoDB import insert_to_db
+from rabbitHelper.rabbitMQ import *
+from storageHelper.s3 import *
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Home route'
+@app.route('/upload', methods=['POST'])
+def upload():
+    response = {}
+    email, music_file = request.form.get('email'), request.files['music']
+
+    try:
+        file_name, file_path = save_file(music_file)
+        hashed_name = generate_hash()
+
+        insert_to_storage(file_path=file_path, file_name=hashed_name)
+        response["s3"] = True
+        insert_to_db(_id=hashed_name, user_email=email)
+        response["mongo"] = True
+        produce_on_rabbit(msg=hashed_name)
+        response["rabbitMQ"] = True
+
+    except Exception as e:
+        logging.error(e)
+
+    return response
 
 
 if __name__ == '__main__':
