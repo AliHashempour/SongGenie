3d968b13fea9550b2c540f133242d62ca4bfd1cd
Ali Hashempour <ali.hashempour@gmail.com>
2024-03-11 21:41:28 +0330

    rafactor: change send email and sleep time

--- mailService/runner.py
+++ mailService/runner.py
@@ -1,7 +1,7 @@
 import time
 
 from dbHelper.mongoDB import select_ready_records, update_status
-from mailService.mailgunWorker import send_simple_message
+from mailService.mailgunWorker import send_email
 from spotifyHelper.spotifyWorker import SpotifyWorker
 
 if __name__ == '__main__':
@@ -10,6 +10,6 @@ if __name__ == '__main__':
         ready_records = select_ready_records()
         for record in ready_records:
             recommendation = spotify_worker.send_recommend_request(song_id=record["songId"])
-            send_simple_message(email=record["email"], text=recommendation)
+            send_email(email=record["email"], text=recommendation)
             update_status(record['_id'], "Done")
-        time.sleep(20)
+        time.sleep(1200)
